afc313191b4a6c8ddf302474497e0dc586e015c1
noizeroobots <noizeroobots@gmail.com>
2021-05-06 22:36:04 +0300

    1

--- src/test/java/ru/fintech/qa/homework/Tests.java
+++ src/test/java/ru/fintech/qa/homework/Tests.java
@@ -1,7 +1,9 @@
 package ru.fintech.qa.homework;
 
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -20,18 +22,15 @@ public class Tests {
     }
 
     @Test
-    public void selectTestHibernatePo(){
+    public void justChecking(){
         System.out.println(new DbClient().getAnimalByName("По"));
     }
 
 
     @Test
-    public void exactly10lines(){
+    public void caseOne(){
         SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
         Session session = sessionFactory.openSession();
-       // Animal animal = session.createNativeQuery("select * from animal", Animal.class).getResultList().get(9);
-//        System.out.println("The name is: " + animal.getName());
-//        Assertions.assertEquals(10,animal.getId());
         List<Animal> id = (List<Animal>) session.createNativeQuery("select * from animal")
         .list();
         Assertions.assertEquals(10, id.size());
@@ -40,18 +39,35 @@ public class Tests {
 
     @Test
     public void cannotAddAStringWithIndex1(){
-        Animal animal = new Animal();
-        animal.setName("Tanos");
-        animal.setAge(25);
-        animal.setType(1);
-        animal.setId(1);
-        animal.setPlace(1);
-        animal.setSex(1);
-        SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        session.save(animal);
-        System.out.println(new DbClient().getAnimalByName("Бусинка"));
-        sessionFactory.close();
+        Transaction transaction = null;
+        try (Session session = HibernateSessionCreator.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the animal object
+            Animal animal = new Animal();
+            animal.setName("Tanos");
+            animal.setAge(25);
+            animal.setType(1);
+            animal.setId(1);
+            animal.setPlace(1);
+            animal.setSex(1);
+            SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
+            session.save(animal);
+            // commit transaction
+            List<Animal> id = (List<Animal>) session.createNativeQuery("select * from animal where id = 1")
+                    .list();
+            Assertions.assertEquals(1,1);
+            transaction.commit();
+
+            sessionFactory.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        System.out.println(new DbClient().getAnimalById(1));
     }
 
     @Test
