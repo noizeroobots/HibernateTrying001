2bd0ed675a71e966c35fe22d24452b3a0497b1c7
noizeroobots <noizeroobots@gmail.com>
2021-05-06 13:53:58 +0300

    3. I can start my homework

--- src/main/java/ru/fintech/qa/homework/utils/DbClient.java
+++ src/main/java/ru/fintech/qa/homework/utils/DbClient.java
@@ -10,7 +10,16 @@ public class DbClient {
         SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
         Session session = sessionFactory.openSession();
         return session.createNativeQuery("select * from animal where \"name\" = '" + name + "'", Animal.class).getResultList().get(0);
-
+    }
+    public Animal getAnimalById(int id){
+        SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        return session.createNativeQuery("select * from animal where id = '" + id + "'", Animal.class).getResultList().get(0);
+    }
+    public Workman getWorkmanById(int id){
+        SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        return session.createNativeQuery("select * from workman where id = '" + id + "'", Workman.class).getResultList().get(0);
     }
 
 }
--- src/test/java/ru/fintech/qa/homework/Tests.java
+++ src/test/java/ru/fintech/qa/homework/Tests.java
@@ -2,12 +2,15 @@ package ru.fintech.qa.homework;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import ru.fintech.qa.homework.utils.Animal;
-import ru.fintech.qa.homework.utils.BeforeUtils;
-import ru.fintech.qa.homework.utils.DbClient;
-import ru.fintech.qa.homework.utils.HibernateSessionCreator;
+import ru.fintech.qa.homework.utils.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Tests {
 
@@ -21,12 +24,52 @@ public class Tests {
         System.out.println(new DbClient().getAnimalByName("По"));
     }
 
+
     @Test
-    public void selectTestHibernateBusinka(){
+    public void exactly10lines(){
         SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
         Session session = sessionFactory.openSession();
-        Animal animal = session.createNativeQuery("select * from animal where \"name\" = 'Бусинка'", Animal.class).getResultList().get(0);
-        System.out.println("The name is: " + animal.getName());
+       // Animal animal = session.createNativeQuery("select * from animal", Animal.class).getResultList().get(9);
+//        System.out.println("The name is: " + animal.getName());
+//        Assertions.assertEquals(10,animal.getId());
+        List<Animal> id = (List<Animal>) session.createNativeQuery("select * from animal")
+        .list();
+        Assertions.assertEquals(10, id.size());
+        sessionFactory.close();
+    }
+
+    @Test
+    public void cannotAddAStringWithIndex1(){
+        Animal animal = new Animal();
+        animal.setName("Tanos");
+        animal.setAge(25);
+        animal.setType(1);
+        animal.setId(1);
+        animal.setPlace(1);
+        animal.setSex(1);
+        SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.save(animal);
+        System.out.println(new DbClient().getAnimalByName("Бусинка"));
+        sessionFactory.close();
+    }
+
+    @Test
+    public void cannotAddAStringWithIndex15(){
+        Animal animal = new Animal();
+        animal.setName("Tanos");
+        animal.setAge(25);
+        animal.setType(1);
+        animal.setId(15);
+        animal.setPlace(1);
+        animal.setSex(1);
+        SessionFactory sessionFactory = HibernateSessionCreator.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(animal);
+        System.out.println(new DbClient().getAnimalByName("Tanos"));
+        session.getTransaction().commit();
+        sessionFactory.close();
     }
 
 }
